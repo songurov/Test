c7165d434afa04532b77cf0cd03961786f5b1e05
curs <songurov1@gmail.com>
2013-12-10 21:24:26 +0200

    asdad
    asd

---
+++ application/views/home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Viteza - {elapsed_time} .ms</title>
+        <link rel="stylesheet" href="<?php echo base_url();?>/media/css/template.css" type="text/css">
+</head>
+<body>
+
+    <article class="container">
+        <section class="menu-stinga">Menu Stinga</section>
+            <section class="container-right">
+                <article class="block-right"></article>
+                
+                <section class="central">
+                    
+                    <article class="error">Error 404</article>
+                
+                    <article class="menu-intern"></article>
+                
+                    <article class="blok-convert"></article>
+                
+                    <article class="blok-calc"></article>
+                    
+                </section>
+                
+                
+            </section>
+    </article>
+
+</body>
+</html>
